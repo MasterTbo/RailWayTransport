1eb655c9911bc6c89ac931af37dda1c9ccdab9bf
MasterTbo <matjele7@gmail.com>
2018-11-11 23:00:04 +0200

    updateFiles

--- Assignment_4/main.js
+++ Assignment_4/main.js
@@ -2,24 +2,41 @@
 
 //Regster the Service Worker
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/RailWayTransport/Assignment_4/service_worker.js', {scope: './'}).then(function(registration){
+    navigator.serviceWorker.register('/service_worker.js', {scope: './'}).then(function(registration){
         console.log('Seervice Worker registrayion successful:', registration);
     }, function(error){
         console.log('Registration failed:', error);
     });
 };
 
+//VUE-Framework
+var app = new Vue({
+    el: '#app',
+    data:{
+
+    },
+    methods: {
+        login: function(){
+            window.location.href="searchTrain.html";
+            //loadMap();
+        }
+    }
+})
+
+
+
+/*
 window.onload = function(){
     loadMap();
 }
 
 //Load map function
 function loadMap(){
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw';
+    mapboxgl.accessToken = 'pk.eyJ1IjoidXdjbGVjdHVyZXIiLCJhIjoiY2ptdWJ6aWt1MGQ4aDN3bzhiM2V1dnRiYyJ9.lWYq773rwVmRzbyHcYAVHw';
     window.map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/strees-v10',
         center: [18.4241, -33.9249], // starting position [longitude, lattitude]
         zoom: 9 // startin zoom
     });
-}
+}*/
