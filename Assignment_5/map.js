9375e206854bbe1fd5757e0393c625064a338cb8
MasterTbo <matjele7@gmail.com>
2018-11-14 23:12:55 +0200

    update

--- Assignment_5/map.js
+++ Assignment_5/map.js
@@ -43,17 +43,92 @@ document.getElementById('map').appendChild(geocoder.onAdd(map));
 */
 
 //New
+//var cityName = document.getElementById('loc').value;
+function loadMap() {
+    //Part 1  
+    //var mymap = L.map('map').setView([-33.9249,18.4241], 13);
+    //L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    //    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    //    maxZoom: 18,
+    //    id: 'mapbox.streets',
+    //    accessToken: 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw'
+    //}).addTo(mymap)
 
-function loadMap() {  
-    var mymap = L.map('map').setView([-33.9249,18.4241], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoidXdjbGVjdHVyZXIiLCJhIjoiY2ptdWJ6aWt1MGQ4aDN3bzhiM2V1dnRiYyJ9.lWYq773rwVmRzbyHcYAVHw'
-    }).addTo(mymap)
+    //Part 2
+    //L.mapbox.accessToken = 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw';
+    //var map = L.mapbox.map('map', 'mapbox.dark')
+    //.setView([-33.9249,18.4241], 10);
+
+    //Part 3
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-33.9249,18.4241],
+        zoom: 13
+    });
+
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken
+    });
+    
+    //Add pointer by locName
+    var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+    mapboxClient.geocoding.forwardGeocode({
+        query: 'Cape Town, South Africa',
+        autocomplete: false,
+        limit: 1
+    })
+    .send()
+    .then(function (response) {
+        if (response && response.body && response.body.features && response.body.features.length) {
+            var feature = response.body.features[0];
+
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: feature.center,
+                zoom: 10
+            });
+            new mapboxgl.Marker()
+                .setLngLat(feature.center)
+                .addTo(map);
+        }
+    });
+
+
+    map.addControl(geocoder);
+
+// After the map style has loaded on the page, add a source layer and default
+// styling for a single point.
+    map.on('load', function() {
+        map.addSource('single-point', {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": []
+            }
+    });
+
+    map.addLayer({
+        "id": "point",
+        "source": "single-point",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        }
+    });
+
+    // Listen for the `result` event from the MapboxGeocoder that is triggered when a user
+    // makes a selection and add a symbol that matches the result.
+    geocoder.on('result', function(ev) {
+        map.getSource('single-point').setData(ev.result.geometry);
+    });
+});
 }
 
+/*
 function searchLocation(){
     var strRegion = document.getElementById('startLocation').innerText;
     var endRegion = document.getElementById('destLocation').innerText;
@@ -70,4 +145,21 @@ geocodingClient
   .then(response => {
     const match = response.body;
   });
-}
+}*/
+/*
+var strRegion = document.getElementById('startLocation').innerText;
+var endRegion = document.getElementById('destLocation').innerText;
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: 'pk.eyJ1IjoidGJvLTAiLCJhIjoiY2ptdDhsZXY1MmM3NTNrbnhiOGJ1bHZoaCJ9.qh77ltMqwFPAZrbUcTNNMw' });
+
+// geocoding with proximity
+geocodingClient
+  .forwardGeocode({
+    query: 'Paris, France',
+    proximity: [stsRegion, endRegion]
+  })
+  .send()
+  .then(response => {
+    const match = response.body;
+  });
+*/
