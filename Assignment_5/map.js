458c2b7cef6df02004fdc40d1c3865e2d7a4d3a8
MasterTbo <matjele7@gmail.com>
2018-11-17 10:40:51 +0200

    update

--- Assignment_5/map.js
+++ Assignment_5/map.js
@@ -1,7 +1,152 @@
-/*var platform = new H.service.Platform({
+var map = L.map('map').setView([-33.91, 18.41], 11)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map)
+
+const appId = 'CVNwhRsBKjQXRlbOZIJs'
+const appCode = '5bgs6vvyBoKP9cAOjBy1gA'
+
+const autocompleteUrl = "http://autocomplete.geocoder.api.here.com/6.2/suggest.json" +
+  "?app_id=" + appId +
+  "&app_code=" + appCode +
+  "&query="
+  
+const geocodeUrl = "https://geocoder.api.here.com/6.2/geocode.json" +
+  "?app_id=" + appId +
+  "&app_code=" + appCode +
+  "&searchtext="
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        startAddress: '',        
+        startPoint: "",
+        destAddress: "",
+        autoCompleteResults: [],
+
+        destLocError: false,
+        startLocError: false,
+        showModal: false,
+        startLocMessage: [],
+        endLocMessage: []        
+    },
+    methods: {
+        locationSearch: function(){
+            //console.log("searchFunc");
+            this.destLocError =  false;
+            this.startLocError =  false;
+            this.showModal = false;
+            this.startLocMessage = [];
+            this.endLocMessage = [];
+
+            if(this.startAddress.length <= 0){
+                this.showModal = true;
+                this.startLocError = true;
+                this.startLocMessage.push({msg:"Please enter start address", date: Date.now()});
+                //console.log("user <= 0");
+            }
+
+            if(this.destAddress.length <= 0){
+                this.showModal = true;
+                this.destLocError = true;
+                this.endLocMessage.push({msg:"Please enter destination address", date: Date.now()});
+                //console.log("user <= 0");
+            }
+        },
+        closeModal: function(){
+            this.destLocError = false;
+            this.startLocError = false;
+            this.startLocMessage = [];
+            this.endLocMessage = [];
+            this.showModal = false;
+            console.log("closeModal");
+        },
+        autocomplete: function () {
+            var _this = this
+            var coordAddres = "";
+            if(this.startAddress.length < 3 && this.destAddress.length < 3) {
+                return false
+            }
+
+            if(this.startAddress.length > 0){
+                console.log("startComplete");
+                fetch(autocompleteUrl + this.startAddress)
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (response) {
+                        _this.autoCompleteResults = response.suggestions
+                    })
+            }
+        
+            if(this.destAddress.length > 0){
+                console.log("destComplete");
+                fetch(autocompleteUrl + this.destAddress)
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (response) {
+                        _this.autoCompleteResults = response.suggestions
+                    })
+            } 
+        },
+        resultSelect: function (result) {
+            var _this = this
+            console.log('result', result)
+            fetch(geocodeUrl + result.label)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (response) {
+                    var location = response.Response.View[0].Result[0].Location.DisplayPosition
+                    _this.startPoint = L.marker([location.Latitude, location.Longitude]).addTo(map)
+                    _this.autoCompleteResults = []                    
+                })
+        }
+    }
+})
+
+//connect to where is my transport, get token.....
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var platform = new H.service.Platform({
     'app_id':'CVNwhRsBKjQXRlbOZIJs',
     'app_code':'5bgs6vvyBoKP9cAOjBy1gA'
-});*/
+});
 
 function moveMapToCapeTown(map){
     map.setCenter({lat:-33.9249, lng:18.4241});
@@ -18,7 +163,7 @@ function putMarker(map){
   /**
    * Boilerplate map initialization code starts below:
    */
-  
+  /*
   //Step 1: initialize communication with the platform
   var platform = new H.service.Platform({
     app_id: 'CVNwhRsBKjQXRlbOZIJs',
@@ -144,9 +289,8 @@ function loadMap(){
 }
 document.getElementById('map').appendChild(geocoder.onAdd(map));
 */
-
+/*
 //New
-<<<<<<< HEAD
 //var cityName = document.getElementById('loc').value;
 function loadMap() {
     //Part 1  
@@ -261,7 +405,7 @@ geocodingClient
     const match = response.body;
   });
 }*/
-<<<<<<< HEAD
+
 /*
 var strRegion = document.getElementById('startLocation').innerText;
 var endRegion = document.getElementById('destLocation').innerText;
@@ -279,7 +423,6 @@ geocodingClient
     const match = response.body;
   });
 */
-=======
 
 /*
 //Lecture code Search Location
@@ -335,4 +478,3 @@ var app = new Vue({
     }
   }
 })*/
->>>>>>> 9e86247dcd234aed0f0a85c83339bc123f60f97a
